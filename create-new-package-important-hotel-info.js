beb2834900c22ef20fbca5ddaadd84f40e9629c4
Season Travel <system.seasonthailand@gmail.com>
2025-07-15 16:02:48 +0700

    Update create-new-package-important-hotel-info.js

--- create-new-package-important-hotel-info.js
+++ create-new-package-important-hotel-info.js
@@ -3,7 +3,7 @@
 
 const hotelMessageInfoArray = [
     {
-        hotelName: "Grand Aston",
+        hotelName: "bndoory",
         messageInfo_p1: "يوم 11 و 12 يوليو غير متوفر في جراند استون",
     },
 ];
