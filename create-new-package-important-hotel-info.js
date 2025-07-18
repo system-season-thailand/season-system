f052b59f50be14ac8f401bc8d1360dfe93b2c794
Season Travel <system.seasonthailand@gmail.com>
2025-07-18 13:56:03 +0700

    Update create-new-package-important-hotel-info.js

--- create-new-package-important-hotel-info.js
+++ create-new-package-important-hotel-info.js
@@ -3,7 +3,7 @@
 
 const hotelMessageInfoArray = [
     {
-        hotelName: "Grand Aston",
+        hotelName: "bndoory",
         messageInfo_p1: "أيام غير متوفرة:",
         messageInfo_p2: "15-17 يوليو",
         messageInfo_p3: "19 يوليو",
