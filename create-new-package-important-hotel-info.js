52d6f1fc2feffb47b959ca8553d38b33e98654af
Season Travel <system.seasonthailand@gmail.com>
2025-09-23 13:51:34 +0700

    Update create-new-package-important-hotel-info.js

--- create-new-package-important-hotel-info.js
+++ create-new-package-important-hotel-info.js
@@ -8,4 +8,10 @@ const hotelMessageInfoArray = [
         messageInfo_p2: "15-17 يوليو",
         messageInfo_p3: "19 يوليو",
     },
+    {
+        hotelName: "Indigo Bali",
+        messageInfo_p1: "في حال حجز ثلاث ليالي او اكثر:",
+        messageInfo_p2: "مساج مجاني لشخصين لمدة 60 دقيقة",
+        messageInfo_p3: "صالح للإستخدام الى 31 أكتوبر 2025",
+    },
 ];
